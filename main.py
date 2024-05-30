28ef7e80080b0a98efaf6b7302cf86b1b700a5dc
morcaprojects <nurmorcaedu@gmail.com>
2024-05-30 15:14:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
-"""Scrapes quotes from the given url.
-
-The url is 'https://quotes.toscrape.com/'.
-"""
 
 import streamlit as st
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
-import pandas as pd
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--window-size=1920x1080')
-chrome_options.add_argument('--disable-gpu')
+with st.echo():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+    from webdriver_manager.core.os_manager import ChromeType
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    @st.cache_resource
+    def get_driver():
+        return webdriver.Chrome(
+            service=Service(
+                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+            ),
+            options=options,
+        )
 
-url = 'https://quotes.toscrape.com/'
-driver.get(url)
+    options = Options()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
 
-quotes = driver.find_elements('xpath', '//span[@class="text"]')
+    driver = get_driver()
+    driver.get("https://edition.cnn.com")
 
-data = []
-for q in quotes:
-    data.append(q.text)
-    
-driver.quit()
-
-df = pd.DataFrame(data, columns=['Quotes'])
-st.dataframe(df)
+    st.code(driver.page_source)
