b5055c2055cea82693dd0bae64f247ea725c75c5
morcaprojects <nurmorcaedu@gmail.com>
2024-05-30 15:24:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
-
 import streamlit as st
+import time
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.firefox import GeckoDriverManager
 
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
+URL = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
+XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
+TIMEOUT = 20
 
-    options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
+st.title("Test Selenium")
+st.markdown("You should see some random Football match text below in about 21 seconds")
 
-    driver = get_driver()
-    driver.get("https://edition.cnn.com")
+firefoxOptions = Options()
+firefoxOptions.add_argument("--headless")
+service = Service(GeckoDriverManager().install())
+driver = webdriver.Firefox(
+    options=firefoxOptions,
+    service=service,
+)
+driver.get(URL)
 
-    
+try:
+    WebDriverWait(driver, TIMEOUT).until(
+        EC.visibility_of_element_located((By.XPATH, XPATH,))
+    )
 
-    matches = driver.find_elements('xpath', '//div[@data-open-link]')
-    links = []
+except TimeoutException:
+    st.warning("Timed out waiting for page to load")
+    driver.quit()
 
-    st.code(matches)
-
-    for i in range(len(matches)):
-       links.append(web + matches[i].get_attribute('data-open-link'))
-       st.markdown(links[i])
-    
+time.sleep(10)
+elements = driver.find_elements_by_xpath(XPATH)
+st.write([el.text for el in elements])
+driver.quit()
